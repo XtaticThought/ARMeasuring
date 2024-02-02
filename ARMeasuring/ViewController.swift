85485c032f06c31d56751b2e4b8d3e7b4e84d624
Xtatic Thought Inc <xtaticthought@gmail.com>
2024-02-02 13:25:48 -0500

    created a circle to appear infront of the camera when you pressed on the screen

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
-
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view.
     }
 
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        guard let currentFrame = sceneView.session.currentFrame else {return}
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        var translationMatrix = matrix_identity_float4x4
+        translationMatrix.columns.3.z = -0.1
+        var modifiedMatrix = simd_mul(transform, translationMatrix)
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = modifiedMatrix
+        self.sceneView.scene.rootNode.addChildNode(sphere)
+    }
 
 }
 
