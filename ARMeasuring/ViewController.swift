ce77a41ee84945e84b324b3eced8ad115e3d618e
Xtatic Thought Inc <xtaticthought@gmail.com>
2024-02-06 11:41:35 -0500

    AR Measring tool. tap on screen for starting position. move phone to desired locatiion and tap again to stop measuring to capture the distance of width height and length.

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 import ARKit
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var distance: UILabel!
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
+    var startingPosition: SCNNode?
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
@@ -20,12 +21,18 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration)
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view.
     }
 
     @objc func handleTap(sender: UITapGestureRecognizer) {
         guard let sceneView = sender.view as? ARSCNView else {return}
         guard let currentFrame = sceneView.session.currentFrame else {return}
+        if self.startingPosition != nil{
+            self.startingPosition?.removeFromParentNode()
+            self.startingPosition = nil
+            return
+        }
         let camera = currentFrame.camera
         let transform = camera.transform
         var translationMatrix = matrix_identity_float4x4
@@ -35,7 +42,28 @@ class ViewController: UIViewController {
         sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
         sphere.simdTransform = modifiedMatrix
         self.sceneView.scene.rootNode.addChildNode(sphere)
+        self.startingPosition = sphere
     }
-
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let startingPosition = self.startingPosition else {return}
+        guard let pointOfView = self.sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let xDistance = location.x - startingPosition.position.x
+        let yDistance = location.y - startingPosition.position.y
+        let zDistance = location.z - startingPosition.position.z
+        DispatchQueue.main.async {
+            self.xLabel.text = String(format: "%.2f", xDistance) + "m"
+            self.yLabel.text = String(format: "%.2f", yDistance) + "m"
+            self.zLabel.text = String(format: "%.2f", zDistance) + "m"
+            self.distance.text = String(format: "%.2f", self.distanceTravelled(x: xDistance, y: yDistance, z: zDistance)) + "m"
+        }
+        
+    }
+    
+    func distanceTravelled(x: Float, y: Float, z: Float) -> Float {
+        return (sqrtf(x*x + y*y + z*z))
+    }
 }
 
